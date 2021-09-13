277818ebe5829d16a7fc8cd4c6681cec9c53e255
DaniilOykin <71172456+DaniilOykin@users.noreply.github.com>
2021-09-13 08:16:21 +0300

    1

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -5,9 +5,33 @@ namespace CourseApp.Tests
     public class DemoTest
     {
         [Fact]
-        public void Test1()
+        public void Test()
         {
             Assert.True(true);
+        }
+
+        [Fact]
+        public void TestIntSum()
+        {
+            int firstNumber = 2;
+            int secondNumber = 3;
+            int expected = 5;
+            var calc = new Calculator();
+            var actual = calc.GetIntSum(firstNumber, secondNumber);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestDoubleSum()
+        {
+            double firstNumber = 2.1;
+            double secondNumber = 3.3;
+            double expected = 5.4;
+            var calc = new Calculator();
+            var actual = calc.GetDoubleSum(firstNumber, secondNumber);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
