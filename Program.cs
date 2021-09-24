ccd90e646f6c693aac05a542a6fd397f06a8f38e
Vladislav Metelev <71036195+vladmet1@users.noreply.github.com>
2021-09-24 20:25:24 +0300

    Task A and B
    function var. 14

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CourseApp
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const double b = 4.2;
+            const double a = 7.2;
+            var funcalc = new Функция();
+            Console.WriteLine("Calculating task A\n");
+            funcalc.CalculateTaskA(1.81, 5.31, 0.7, a, b);
+            double[] nums = new double[] { 2.4, 2.8, 3.9, 4.7, 3.16 };
+            Console.WriteLine("Calculating task B\n");
+            funcalc.CalculateTaskB(nums, a, b);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ функция.cs
@@ -0,0 +1,31 @@
+﻿namespace CourseApp
+{
+    using System;
+
+    public class Функция
+    {
+        public void Calculate(double a, double b, double x)
+        {
+            double top = Math.Sqrt(Math.Abs(a - (b * x)));
+            double bottom = Math.Sqrt(Math.Pow(Math.Log(10), 3));
+            double y = Math.Round(top / bottom, 3);
+            Console.WriteLine(y);
+        }
+
+        public void CalculateTaskA(double xStart, double xEnd, double dX, double a, double b)
+        {
+            for (double x = xStart; x < xEnd; x += dX)
+            {
+                Calculate(a, b, x);
+            }
+        }
+
+        public void CalculateTaskB(double[] nums, double a, double b)
+        {
+            foreach (double number in nums)
+            {
+                Calculate(a, b, number);
+            }
+        }
+    }
+}
