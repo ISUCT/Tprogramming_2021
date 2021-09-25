56118641762213c4b6527816ba01d47ee1a8e07b
swbonjour <71038541+swbonjour@users.noreply.github.com>
2021-09-25 15:24:19 +0300

    TestFormula

---
+++ CourseApp.Tests/TestFormula.cs
@@ -0,0 +1,48 @@
+using System;
+using Xunit;
+using Formula;
+
+namespace TestFormula
+{
+    public class TestFormula
+    {
+        [Fact]
+        public void Part_A_With_D()
+        {
+            Formula10 f = new Formula10();
+
+            double xb = 0.2;
+            double xe = 0.95;
+            double dx = 0.15;
+            double a = 0.05;
+            double b = 0.06;
+            double[] f_res = new double[5] { 40.90742715368491, 12.067634851044216, 5.285233882132713, 2.626773674898777, 1.2744671426673164 };
+
+            double[] res = f.PartA(xb, xe, dx, a, b);
+
+            for(int i=0; i<res.Length; i++)
+            {
+                Assert.Equal(res[i], f_res[i]);
+            }
+        }
+
+        [Fact]
+        public void Part_B()
+        {
+            Formula10 f = new Formula10();
+
+            double a = 0.05;
+            double b = 0.06;
+            double[] x_nums = new double[5] { 0.15, 0.26, 0.37, 0.48, 0.56 };
+            double[] f_res = new double[5] { 77.58958649972757, 23.128832605187945, 10.660338909522668, 5.83682718123348, 3.969028946438846 };
+
+            double[] res = f.PartB(x_nums, a, b);
+
+            for(int i=0; i<res.Length; i++)
+            {
+                Assert.Equal(res[i], f_res[i]);
+            }
+
+        }
+    }
+}
