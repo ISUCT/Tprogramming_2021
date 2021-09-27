6b958589ebd76d5245b5f48e1406c528842eb436
alexthvest <alexthvest@gmail.com>
2021-09-27 12:12:29 +0300

    Initial RPG Saga project setup

---
+++ AkvelonSaga.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AkvelonSaga.Client
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Client!");
+        }
+    }
+}
---
+++ AkvelonSaga.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AkvelonSaga.Core
+{
+    public class Class1
+    {
+    }
+}
