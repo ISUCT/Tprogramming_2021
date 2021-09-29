cb699ca7b3a6dc99f034da48bf7481d469060a05
solnyshk0 <71177294+solnyshk0@users.noreply.github.com>
2021-09-29 22:13:18 +0300

    Add files via upload
    Вот написала саму функцию, с написанием тестов проблемы

---
+++ Lab1_function_.cs
@@ -0,0 +1,31 @@
+﻿namespace CourseApp
+{
+    using System;
+
+    public class Lab1_function_
+    {
+        public void Calculate(double b, double x, double a)
+        {
+            double top = Math.Pow(b, 3) + Math.Pow(Math.Sin(a * x), 2);
+            double bottom = Math.Acos(x * b * x) + Math.Exp(-x / 2);
+            double y = top / bottom;
+            Console.WriteLine(y);
+        }
+
+        public void CalculateTaskA(double xStart, double xEnd, double dX, double b, double a)
+        {
+            for (double x = xStart; x < xEnd; x += dX)
+            {
+                Calculate(b, x, a);
+            }
+        }
+
+        public void CalculateTaskB(double[] nums, double b, double a)
+        {
+            foreach (double number in nums)
+            {
+                Calculate(b, number, a);
+            }
+        }
+    }
+}
