ac8536a02fe181488b782f7ecffe05cf564cb380
Alexey Borsh <spectlol@yandex.ru>
2021-10-01 15:38:46 +0300

    v1

---
+++ ConsoleApp1/Example.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp1
+
+{
+    interface IDateProvider
+    {
+        string GetDate();
+    }
+    interface IDateProcessor
+    {
+        void ProcessorDate(IDateProvider dateProvider);
+    }
+
+    class ConsoleDateProcessor : IDateProcessor
+    {
+        void IDateProcessor.ProcessorDate(IDateProvider dateProvider)
+        {
+            Console.WriteLine(dateProvider.GetDate());
+        }
+    }
+
+    class DbDateProvider : IDateProvider
+    {
+        public string GetDate()
+        {
+            return "Данные из БД";
+        }
+    }
+
+    class FileProvider : IDateProvider
+    {
+        public string GetDate()
+        {
+            return "Данные из файла";
+        }
+    }
+
+    class APIDateProvider : IDateProvider
+    {
+        public string GetDate()
+        {
+            return "Данные из API";
+        }
+    }
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            IDateProcessor dateProcessor = new ConsoleDateProcessor();
+            dateProcessor.ProcessorDate(new DbDateProvider());
+            dateProcessor.ProcessorDate(new FileProvider());
+            dateProcessor.ProcessorDate(new APIDateProvider());
+        }
+    }
+}
