d4625814bcf760f024698cfe669ad49e6883896d
Vladislav Metelev <71036195+vladmet1@users.noreply.github.com>
2021-10-03 20:36:15 +0300

    update/fix
    Function var. 14

--- Function.cs
+++ Function.cs
@@ -1,15 +1,15 @@
-﻿namespace CourseApp
+namespace CourseApp
 {
     using System;
 
-    public class Функция
+    public class Function
     {
         public void Calculate(double a, double b, double x)
         {
             double top = Math.Sqrt(Math.Abs(a - (b * x)));
             double bottom = Math.Sqrt(Math.Pow(Math.Log(10), 3));
-            double y = Math.Round(top / bottom, 3);
-            Console.WriteLine(y);
+            double y = top / bottom;
+            Console.WriteLine(Math.Round(y, 3));
         }
 
         public void CalculateTaskA(double xStart, double xEnd, double dX, double a, double b)
