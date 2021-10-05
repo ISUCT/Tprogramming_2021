70a9e29685ea2e46ed7b314135ccd03f802c7f78
solnyshk0 <71177294+solnyshk0@users.noreply.github.com>
2021-10-05 20:44:11 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
         {
             const double a = 1.2;
             const double b = 0.48;
-            var funcalc = new Lab1_function_();
+            var funcalc = new Lab1Function();
             Console.WriteLine("Task A\n");
             funcalc.CalculateTaskA(0.7, 2.2, 0.3, b, a);
             double[] nums = new double[] { 0.25, 0.36, 0.56, 0.94, 1.28 };
