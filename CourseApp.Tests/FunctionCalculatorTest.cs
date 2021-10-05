026382a0b928aaa8c304e0a9e6340dfc26d84599
Alexey Borsh <spectlol@yandex.ru>
2021-10-05 17:08:18 +0300

    up

---
+++ CourseApp.Tests/FunctionCalculatorTest.cs
@@ -0,0 +1,14 @@
+﻿namespace CourseApp.Tests
+{
+    using Xunit;
+
+    public class FunctionCalculatorTest
+    {
+        [Fact]
+
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
