24626ba6bb50e84b40f5d310464bcea80171decb
Elllen <71223333+LenaNaumova@users.noreply.github.com>
2021-10-06 19:07:15 +0300

    Просто калькулятор

---
+++ TestCalcIgor.cs
@@ -0,0 +1,74 @@
+﻿namespace CourseApp.Tests
+{
+    using Xunit;
+
+    public class TestCalcIgor
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void TestIntSum()
+        {
+            int firstNumber = 2;
+            int secondNumber = 3;
+            int expected = 5;
+            var calc = new CalcIgor();
+            var actual = calc.GetSum(firstNumber, secondNumber);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestDoubleSum()
+        {
+            double firstNumber = 2.1;
+            double secondNumber = 3.3;
+            double expected = 5.4;
+            var calc = new CalcIgor();
+            var actual = calc.GetSum(firstNumber, secondNumber);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestIntProduct()
+        {
+            int firstNumber = 2;
+            int secondNumber = 3;
+            int expected = 6;
+            var calc = new CalcIgor();
+            var actual = calc.GetProduct(firstNumber, secondNumber);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestDoubleQuotient()
+        {
+            double firstNumber = 5;
+            double secondNumber = 2;
+            double expected = 2.5;
+            var calc = new CalcIgor();
+            var actual = calc.GetQuotient(firstNumber, secondNumber);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestDoubleQuotientNull()
+        {
+            var calc = new CalcIgor();
+            double firstNumber = 5;
+            double secondNumber = 0;
+            double expected = double.PositiveInfinity;
+
+            var actual = calc.GetQuotient(firstNumber, secondNumber);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
