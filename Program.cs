aeb225099dbb59b603724358c54639b1d68afaec
Elllen <71223333+LenaNaumova@users.noreply.github.com>
2021-10-06 19:10:18 +0300

    СЛОЖНАЯ ФУНКЦИЯ (пусть проверит Е.Е)

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CourseApp
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const double b = 1.1;
+            const double a = 2.0;
+            var functionCalculator = new FunctionCalculator();
+            Console.WriteLine("Calculating task A:\n");
+
+            foreach (double num in functionCalculator.CalculateTaskA(a, 0.08, 1.08, 0.2, b))
+            {
+                Console.WriteLine(num);
+            }
+
+            double[] nums = new double[] { 0.1, 0.3, 0.4, 0.45, 0.65 };
+            Console.WriteLine("\nCalculating task B:\n");
+            foreach (double num in functionCalculator.CalculateTaskB(a, nums, b))
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestFunctionCalculator.cs
@@ -0,0 +1,66 @@
+﻿namespace CourseApp.Tests
+{
+    using Xunit;
+
+    public class TestFunctionCalculator
+    {
+        [Fact]
+        public void TestRight()
+        {
+            var calc = new FunctionCalculator();
+
+            double firstNumber = 2;
+            double secondNumber = 6;
+            double thirdNumber = 7;
+            double expected = 1.7;
+
+            double actual = calc.Calculate(firstNumber, secondNumber, thirdNumber);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestQuotientNull()
+        /*знаменатель больше нуля(на ноль делить нельзя)*/
+        {
+            var calc = new FunctionCalculator();
+
+            double firstNumber = 0;
+            double secondNumber = 5;
+            double thirdNumber = 0;
+            double expected = double.PositiveInfinity;
+
+            double actual = calc.Calculate(firstNumber, secondNumber, thirdNumber);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestPositiveBase()
+        /*основание логарифма (а) должно быть больше нуля*/
+        {
+            var calc = new FunctionCalculator();
+
+            double firstNumber = -1;
+            double secondNumber = 5;
+            double thirdNumber = 7;
+            double expected = double.PositiveInfinity;
+
+            double actual = calc.Calculate(firstNumber, secondNumber, thirdNumber);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestAMoreOne()
+        /*основание логарифма (а) не должно равняться 1 по определению*/
+        {
+            var calc = new FunctionCalculator();
+
+            double firstNumber = 1;
+            double secondNumber = 5;
+            double thirdNumber = 7;
+            double expected = double.PositiveInfinity;
+
+            double actual = calc.Calculate(firstNumber, secondNumber, thirdNumber);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
