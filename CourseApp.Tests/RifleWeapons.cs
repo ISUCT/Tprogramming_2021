b6cd923e9fe27e564824e71d2b1ed99fd8ca1f2f
Ihate Shestakov <baranka2003@yandex.ru>
2021-10-12 23:25:52 +0300

    Calculator 1.1

---
+++ CourseApp.Tests/RifleWeapons.cs
@@ -0,0 +1,13 @@
+﻿namespace CourseApp.Tests
+{
+    using Xunit;
+
+    public class RifleWeapons
+    {
+        [Fact]
+        public void GetModel()
+        {
+            var pistol = new NewWeapon("pistol", "Bersa Thunder 380", 9.17);
+        }
+    }
+}
