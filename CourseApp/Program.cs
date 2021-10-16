601c736722ab582ddae9e8820925606a7ee8a595
Auzet <alexpolehin28@gmail.com>
2021-10-16 03:24:52 +0300

    Complete Lab1

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -6,8 +6,26 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine($"Hello world");
-            Console.ReadLine();
+            double xn = 0.08;
+            double xk = 1.08;
+            double xd = 0.2;
+            double[] values = { 0.1, 0.3, 0.4, 0.45, 0.65 };
+            Tasks tasksAB = new Tasks();
+
+            double[] taskA = tasksAB.CalculateTask(xn, xk, xd);
+            double[] taskB = tasksAB.CalculateTask(values);
+
+            foreach (double x in taskA)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("-----------------------------");
+
+            foreach (double x in taskB)
+            {
+                Console.WriteLine(x);
+            }
         }
     }
 }
---
+++ CourseApp/Tasks.cs
@@ -0,0 +1,31 @@
+﻿namespace CourseApp
+{
+    public class Tasks
+    {
+        private Function _function = new Function();
+
+        public double[] CalculateTask(double xn, double xk, double xd)
+        {
+            int k = 0;
+            double[] taskA = new double[5];
+            for (double i = xn; i < xk; i += xd)
+            {
+                taskA[k++] = _function.Calculate(i);
+            }
+
+            return taskA;
+        }
+
+        public double[] CalculateTask(double[] values)
+        {
+            double[] taskB = new double[5];
+            int k = 0;
+            foreach (double i in values)
+            {
+                taskB[k++] = _function.Calculate(i);
+            }
+
+            return taskB;
+        }
+    }
+}
