bcca4ec96dc3c678f2b21680cbc6da92d3e9be15
Alexander <71167144+SanyaKirilv@users.noreply.github.com>
2021-10-22 11:01:54 +0300

    Split UnitTests to 2 files

--- CourseApp.Tests/UnitTests.cs
+++ CourseApp.Tests/UnitTests.cs
@@ -2,39 +2,10 @@ namespace CourseApp.Tests
 {
     using System;
     using CourseApp.Program;
-    using CourseApp.Program.Input;
     using Xunit;
 
     public class UnitTests
     {
-        [Theory]
-        [InlineData("2", 0, 3, true)]
-        [InlineData("-1", 0, 3, false)]
-        [InlineData("r", 0, 3, false)]
-        [InlineData("-1.2", -2, -1, false)]
-        [InlineData("4.1", 4, 5, false)]
-        [InlineData(" ", 4, 4, false)]
-        public void TestInputInt(string item, int minValue, int maxValue, bool expected)
-        {
-            var input = new InputValues();
-            var actual = input.InputInt(minValue, maxValue, item);
-            Assert.Equal(expected, actual);
-        }
-
-        [Theory]
-        [InlineData("2,2", 0, 3, true)]
-        [InlineData("-1,5", 0, 3, false)]
-        [InlineData("r", 0, 3, false)]
-        [InlineData("-1", -2, -1, true)]
-        [InlineData("4,1", 4, 5, true)]
-        [InlineData(" ", 4, 4, false)]
-        public void TestInputDouble(string item, int minValue, int maxValue, bool expected)
-        {
-            var input = new InputValues();
-            var actual = input.InputDouble(minValue, maxValue, item);
-            Assert.Equal(expected, actual);
-        }
-
         [Theory]
         [InlineData(2021, 10, 15, 2002, 4, 24, "19 5 21")]
         [InlineData(2021, 10, 15, 2006, 4, 21, "15 5 24")]
