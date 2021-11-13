e02f78cf4207611da48daf80e8284e6dedcf76d2
Alexander <71167144+SanyaKirilv@users.noreply.github.com>
2021-11-13 11:38:48 +0300

    Fix Test 3

--- CourseApp.Tests/InputValueUnitTests.cs
+++ CourseApp.Tests/InputValueUnitTests.cs
@@ -45,7 +45,7 @@ namespace CourseApp.Tests
 
         [Theory]
         [InlineData("2,2", 0, 3, true)]
-        [InlineData("-1,5", 0, 3, false)]
+        [InlineData("-1.5", 0, 3, false)]
         [InlineData("r", 0, 3, false)]
         [InlineData("-1", -2, -1, true)]
         [InlineData("4,1", 4, 5, true)]
