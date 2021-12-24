bbc231b92145bdb6b6cc077ee0eeea11b10e8079
Mark_Rumjantsev <mrk.ru@list.ru>
2021-12-24 19:26:39 +0300

    phrase

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -7,7 +7,6 @@ namespace CourseApp
     {
         public static void Main(string[] args)
         {
-            string[] phraseArray = { "Грустно:(", "Как же печально:(", "Жалко животинку:(" };
             PigFamily pigOne = new Pig();
             PigFamily pigTwo = new Pig("свинка Пеппа", 9, 99);
             Boar boarOne = new Boar("кабанчик Кабанистый", 1, 5, 7);
