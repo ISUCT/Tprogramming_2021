f1bd4d10bb24a2dd351e0cdfbffb420ab760cc9e
Alexander <71167144+SanyaKirilv@users.noreply.github.com>
2021-12-28 15:18:41 +0300

    Update InputValueUnitTests.cs

--- CourseApp.Tests/InputValueUnitTests.cs
+++ CourseApp.Tests/InputValueUnitTests.cs
@@ -18,10 +18,10 @@ namespace CourseApp.Tests
         }
 
         [Theory]
-        [InlineData(0,1, -1, 2, true)]
-        [InlineData(0,5, 1, 2, false)]
+        [InlineData(0.1, -1, 2, true)]
+        [InlineData(0.5, 1, 2, false)]
         [InlineData(1, 0, 2, true)]
-        [InlineData(1,2, 1, 2, true)]
+        [InlineData(1.2, 1, 2, true)]
         public void TestCheckValuesInputDouble(double input, double min, double max, bool expected)
         {
             var check = new CheckValues();
@@ -33,8 +33,8 @@ namespace CourseApp.Tests
         [InlineData("2", 0, 3, true)]
         [InlineData("-1", 0, 3, false)]
         [InlineData("r", 0, 3, false)]
-        [InlineData("-1,2", -2, -1, false)]
-        [InlineData("4,1", 4, 5, false)]
+        [InlineData("-1.2", -2, -1, false)]
+        [InlineData("4.1", 4, 5, false)]
         [InlineData(" ", 4, 4, false)]
         public void TestInputInt(string item, int minValue, int maxValue, bool expected)
         {
@@ -44,11 +44,11 @@ namespace CourseApp.Tests
         }
 
         [Theory]
-        [InlineData("2,2", 0, 3, true)]
-        [InlineData("-1,5", 0, 3, false)]
+        [InlineData("2.2", 0, 3, true)]
+        [InlineData("-1.5", 0, 3, false)]
         [InlineData("r", 0, 3, false)]
         [InlineData("-1", -2, -1, true)]
-        [InlineData("4,1", 4, 5, true)]
+        [InlineData("4.1", 4, 5, true)]
         public void TestInputDouble(string item, int minValue, int maxValue, bool expected)
         {
             var input = new InputValues();
