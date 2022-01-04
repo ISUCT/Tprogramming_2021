534fb2cf10de0a5235aa361784412c1bb78fdc3b
Stepan_Bugasov <72213274+Bugfasov@users.noreply.github.com>
2022-01-04 13:21:15 +0300

    Age_fixed2

--- CourseApp/MainFile.cs
+++ CourseApp/MainFile.cs
@@ -11,8 +11,8 @@ namespace CourseApp
             var dateToday = DateTime.Today;
             var date = dateClass.CalculateAge(birthDay, dateToday);
             var days = dateClass.CalculateDaysToBirthDay(birthDay, dateToday);
-            Console.WriteLine($"Your age: {date.Item1} y, {date.Item2} m and {date.Item3} d");
-            Console.WriteLine($"Days to bd: {days.Item1} m, {days.Item2} d");
+            Console.WriteLine($"Your age: {date.Item1} year, {date.Item2} month and {date.Item3} day");
+            Console.WriteLine($"Days to bd: {days.Item1} month, {days.Item2} day");
             Console.ReadLine();
         }
     }
--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 ## Tprogramming 2021
-### For Date Task
+### This Branch for Class Task
 In this program you can:
   - Set your birthDay
   - Get your age
   - Get date to your birthDay
-### For Calculate Task
- In this program you can:
-  - Operate with default values
