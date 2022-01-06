c148b0d8dd926a1b78559be7a88916b238540cbb
pkubysheva <71191097+pkubysheva@users.noreply.github.com>
2022-01-06 17:48:54 +0300

    Add files via upload

---
+++ Tprogramming_2021/CourseApp/Program.cs
@@ -0,0 +1,27 @@
+﻿ namespace CourseApp
+{
+    using System;
+    /* using System.Collections.Generic;*/
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] phraseArray = { "Нет свинки" };
+            Pig pigOne = new Pig();
+            Pig pigTwo = new Pig("Свинка", 9, 99);
+            Deer deerOne = new Deer("Олень 1", 1, 5, 7);
+            Deer deerTwo = new Deer("Олень 2", 3, 2, 8);
+            Animals[] animals = new Animals[] { pigOne, pigTwo, deerOne, deerTwo };
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+
+            Console.Write(pigTwo.Died());
+            Console.WriteLine(pigTwo.MakePhrase(phraseArray));
+            Console.Write(deerTwo.Died());
+            Console.WriteLine(deerTwo.MakePhrase(phraseArray));
+        }
+    }
+}
