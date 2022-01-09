3f3e792d396a3cb58fd9801b686b7352fa8cb534
Denis <71184288+Unp1ugged@users.noreply.github.com>
2022-01-10 00:43:55 +0300

    Add files via upload

---
+++ testInterface/Example.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace testInterface
+
+{
+
+    class ConsoleDateProcessor : IDateProcessor
+    {
+        void IDateProcessor.ProcessorDate(IDataProvider dateProvider)
+        {
+            Console.WriteLine(dateProvider.GetDate());
+        }
+    }
+
+    class DbDateProvider : IDataProvider
+    {
+        public string GetDate()
+        {
+            return "Данные из БД";
+        }
+    }
+
+    class FileProvider : IDataProvider
+    {
+        public string GetDate()
+        {
+            return "Данные из файла";
+        }
+    }
+
+    class APIDataProvider : IDataProvider
+    {
+        public string GetDate()
+        {
+            return "Данные из API";
+        }
+    }
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            IDateProcessor dateProcessor = new ConsoleDateProcessor();
+            dateProcessor.ProcessorDate(new DbDateProvider());
+            dateProcessor.ProcessorDate(new FileProvider());
+            dateProcessor.ProcessorDate(new APIDataProvider());
+        }
+    }
+}
