2e4d6fbb34a3801fb93970a8087e4cec4c85ec74
Creny <artem.0110@mail.ru>
2022-01-13 20:40:26 +0300

    TaskOne

---
+++ CourseApp.Tests/TestTask1.cs
@@ -0,0 +1,25 @@
+﻿namespace CourseApp.Tests
+{
+    using Xunit;
+
+    public class TestTask1
+    {
+        private readonly Func _func;
+
+        public TestTask1()
+        {
+            _func = new Func();
+        }
+
+        [Theory]
+        [InlineData(1.4, )]
+        [InlineData(0, -1)]
+        [InlineData(-2.1, -1)]
+        [InlineData()]
+        public void RunTest(double x, double expected)
+        {
+            double ans = _func.Foo(x);
+            Assert.Equal(expected, ans, 3);
+        }
+    }
+}
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -6,8 +6,67 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine($"Hello world");
+            Func f = new Func();
+            Console.WriteLine(f.Foo(1.4));
+            Console.WriteLine(f.Foo(0));
+            Console.WriteLine(f.Foo(-2.1));
+            Console.WriteLine(f.Foo(10));
+            Console.WriteLine(f.Foo(0.0003));
+            double x_s = 1.25;
+            double x_f = 2.75;
+            double dx = 0.3;
+            double[] ans_a = new double[(int)((x_f - x_s) / dx) + 1];
+            TaskA(ref ans_a, x_s, x_f, dx);
+            double[] arg = { 2.2, 3.78, 4.51, 6.58, 1.2 };
+            double[] ans_b = new double[5];
+            TaskB(ref ans_b, arg);
+            Console.WriteLine("Task A:");
+            ShowUp(ans_a);
+            Console.WriteLine("Task B:");
+            ShowUp(ans_b);
             Console.ReadLine();
+        }
+
+        public static double Foo(double x)
+        {
+            if (x <= 0)
+            {
+                return -1;
+            }
+
+            double y;
+            const double a = 2.0;
+            const double b = 0.95;
+            y = (1 + Math.Pow(Math.Log(x / a, 10), 2)) / (b - Math.Pow(Math.E, x / a));
+            return y;
+        }
+
+        private static void ShowUp(double[] m)
+        {
+            for (int i = 0; i < m.Length - 1; i++)
+            {
+                Console.Write($"{m[i]}, ");
+            }
+
+            Console.WriteLine(m[m.Length - 1]);
+        }
+
+        private static void TaskA(ref double[] m, double x_s, double x_f, double dx)
+        {
+            int i = 0;
+            for (double x = x_s; x <= x_f; x += dx)
+            {
+                m[i] = Foo(x);
+                i++;
+            }
+        }
+
+        private static void TaskB(ref double[] ans, double[] m)
+        {
+            for (int i = 0; i < m.Length; i++)
+            {
+                ans[i] = Foo(m[i]);
+            }
         }
     }
 }
