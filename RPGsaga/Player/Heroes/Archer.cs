4e45b9b13bc3621b23d4b0dbdd554368b3c2ed50
Alexey <37597550+Borshecs@users.noreply.github.com>
2022-02-07 00:46:26 +0300

    Delete RPGsaga directory

--- RPGsaga/Player/Heroes/Archer.cs
+++
@@ -1,9 +0,0 @@
-namespace RPGsaga.Player
-{
-    public class Archer : Player
-    {
-        public Archer(string name, double health, double strength) : base(name, health, strength)
-        {
-        }
-    }
-}
--- RPGsaga/Player/Heroes/Mage.cs
+++
@@ -1,9 +0,0 @@
-namespace RPGsaga.Player
-{
-    public class Mage : Player
-    {
-        public Mage(string name, double health, double strength) : base(name, health, strength)
-        {
-        }
-    }
-}
