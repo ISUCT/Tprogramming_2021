3fe11251d3804dfe1565d535dc64f22984165995
DaniilOykin <71172456+DaniilOykin@users.noreply.github.com>
2022-02-09 23:54:46 +0300

    1

--- Abilities/IAbility.cs
+++ Abilities/IAbility.cs
@@ -1,6 +1,8 @@
 namespace RpgSaga.Abilities;
 
 public interface IAbility {
+    string Damage_Buff { get;}
+    string Strength_Buff { get;}
     string Name { get;}
     int Duration { get;}
     int Damage { get;}
@@ -14,7 +16,11 @@ public class FireArrows : IAbility{
         this.Duration = 9999;
         this.Damage = 2;
         this.Stun = false;
+        this.Damage_Buff = "Fire Arrows";
+        this.Strength_Buff = "Fire Arrows";
     }
+    public string Damage_Buff { get;}
+    public string Strength_Buff { get;}
     public string Name { get;}
     public int Duration { get;}
     public int Damage { get;}
