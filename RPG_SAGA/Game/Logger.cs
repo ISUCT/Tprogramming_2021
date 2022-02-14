c329c738b64cc06d8d6b0dd85bc635a942bdad6a
DaniilOykin <71172456+DaniilOykin@users.noreply.github.com>
2022-02-14 07:54:18 +0300

    1

--- RPG_SAGA/Game/Logger.cs
+++ RPG_SAGA/Game/Logger.cs
@@ -16,32 +16,32 @@ public class Logger
 
         public void Versus(Player player1, Player player2)
         {
-            Console.WriteLine($"({player1.GetType}) {player1.Name} vs ({player2.GetType}) {player2.Name}");
+            Console.WriteLine($"({player1.GetType().Name}) {player1.Name} vs ({player2.GetType().Name}) {player2.Name}");
         }
 
         public void Attack(Player player1, Player player2, int damage)
         {
-            Console.WriteLine($"({player1.GetType}) {player1.Name} Attack ({player2.GetType}) {player2.Name} and deal {damage} damage");
+            Console.WriteLine($"({player1.GetType().Name}) {player1.Name} attack ({player2.GetType().Name}) {player2.Name} and deal {damage} damage");
         }
 
         public void Attack(Player player1, Player player2, string ability)
         {
-            Console.WriteLine($"({player1.GetType}) {player1.Name} use {ability} on ({player2.GetType}) {player2.Name}");
+            Console.WriteLine($"({player1.GetType().Name}) {player1.Name} use {ability} on ({player2.GetType().Name}) {player2.Name}");
         }
 
         public void Attack(Player player1, Player player2, int damage, string ability)
         {
-            Console.WriteLine($"({player1.GetType}) {player1.Name} attack ({player2.GetType}) {player2.Name} with {ability} and deal {damage} damage");
+            Console.WriteLine($"({player1.GetType().Name}) {player1.Name} attack ({player2.GetType().Name}) {player2.Name} with {ability} and deal {damage} damage");
         }
 
         public void Death(Player player)
         {
-            Console.WriteLine($"({player.GetType}) {player.Name} is dead");
+            Console.WriteLine($"({player.GetType().Name}) {player.Name} is dead");
             Console.WriteLine();
         }
 
         public void End(Player player)
         {
-            Console.WriteLine($"The game is over. Winner is ({player.GetType}) {player.Name}");
+            Console.WriteLine($"The game is over. Winner is ({player.GetType().Name}) {player.Name}");
         }
 }
