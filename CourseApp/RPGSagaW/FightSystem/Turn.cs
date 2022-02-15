e4d2fc532da1c3aeb05e81224244baa95b25fa0a
swbonjour <71038541+swbonjour@users.noreply.github.com>
2022-02-15 18:43:17 +0300

    turn

---
+++ CourseApp/RPGSagaW/FightSystem/Turn.cs
@@ -0,0 +1,21 @@
+namespace RPGSagaW.FightSystem;
+
+public static class Turn
+{
+    public static int RandomFirstTurn()
+    {
+        return Random.Shared.Next(0, 1);
+    }
+
+    public static void ChangeTurn(ref int turn)
+    {
+        if (turn == 0)
+        {
+            turn = 1;
+        }
+        else
+        {
+            turn = 0;
+        }
+    }
+}
